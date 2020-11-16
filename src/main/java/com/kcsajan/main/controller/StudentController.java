608fa2fd27434c82212c939455ebf68d796903c1
Sajan Kc <sajan1996kc@gmail.com>
2020-11-16 21:03:07 +0545

    GetMapping added

--- src/main/java/com/kcsajan/main/controller/StudentController.java
+++ src/main/java/com/kcsajan/main/controller/StudentController.java
@@ -1,9 +1,19 @@
 package com.kcsajan.main.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kcsajan.main.model.Student;
 import com.kcsajan.main.repository.StudentRepository;
 
 @RestController
@@ -12,6 +22,29 @@ public class StudentController {
 
 	@Autowired
 	StudentRepository studentRepository;
-	
-	
+
+	@GetMapping("/students")
+	public ResponseEntity<List<Student>> getStudents() {
+		try {
+			List<Student> students = studentRepository.findAll();
+			if (students.isEmpty())
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity<>(students, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/students/{id}")
+	public ResponseEntity<Student> getStudent(@PathVariable("id") Integer id) {
+		try {
+			Optional<Student> student = studentRepository.findById(id);
+			if (student.isPresent())
+				return new ResponseEntity<>(student.get(), HttpStatus.OK);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
