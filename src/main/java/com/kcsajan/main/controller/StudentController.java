fac6cf9f82a35f0fcb154afb86e21ac156dea873
Sajan Kc <sajan1996kc@gmail.com>
2020-11-16 21:42:02 +0545

    PutMapping added

--- src/main/java/com/kcsajan/main/controller/StudentController.java
+++ src/main/java/com/kcsajan/main/controller/StudentController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kcsajan.main.model.Student;
 import com.kcsajan.main.repository.StudentRepository;
+
+import net.bytebuddy.implementation.FieldAccessor.FieldNameExtractor;
 
 @RestController
 @RequestMapping("/api")
@@ -53,6 +56,26 @@ public class StudentController {
 		try {
 			Student studentData = studentRepository.save(student);
 			return new ResponseEntity<>(studentData, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/students/{id}")
+	public ResponseEntity<Student> updateStudents(@PathVariable("id") Integer id, @RequestBody Student student) {
+		try {
+			Optional<Student> studentData = studentRepository.findById(id);
+			if (studentData.isPresent()) {
+				Student updatedStudent = studentData.get();
+				updatedStudent.setRollNo(student.getRollNo());
+				updatedStudent.setAddress(student.getAddress());
+				updatedStudent.setEmail(student.getEmail());
+				updatedStudent.setFname(student.getFname());
+				updatedStudent.setLname(student.getLname());
+				updatedStudent.setPhone(student.getPhone());
+				return new ResponseEntity<>(studentRepository.save(updatedStudent), HttpStatus.OK);
+			}
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
