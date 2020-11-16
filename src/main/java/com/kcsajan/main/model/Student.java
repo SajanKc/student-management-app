cd5891de011b3b803e64a65451c07ab550ae0428
Sajan Kc <sajan1996kc@gmail.com>
2020-11-16 21:26:24 +0545

    DeleteMapping added

--- src/main/java/com/kcsajan/main/model/Student.java
+++ src/main/java/com/kcsajan/main/model/Student.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Student {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
 	private int rollNo;
